d0973f4c019b6aee573aa30ed718f3cae103b2a7
Omid Faqiri Poya <165857481+omidfaqiri@users.noreply.github.com>
2025-08-20 18:27:39 -0500

    Delete pcep directory

--- pcep/08032025/assign1.py
+++
@@ -1,4 +0,0 @@
-length = int(input("what is the length=?"))
-width = int(input("what is the width=?"))
-print((length) * (width))
-print(2 * (length) * (width))
--- pcep/08032025/assign2.py
+++
@@ -1,8 +0,0 @@
-#var = 35
-#binary_num = ""
-#while var > 0:
- #   remainder = var % 2
-  #  binary_num +=str(remainder)
-   # var = var // 2
-#print(binary_num)
-
